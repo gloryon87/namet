8fb8b938678f8abca426afc264e90f4f2ec77679
Slava Tiutiunnyk <gloryon87@gmail.com>
2024-02-03 20:44:37 +0200

    added metadata for home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,14 @@ import React from 'react'
 import Login from './components/Login/Login'
 import Logout from './components/Logout/Logout'
 import validateToken from './API/validateToken'
+import { metadata } from './layout'
 
 const url = process.env.REACT_APP_SERVER_URL || ''
+
+metadata = {
+  title: 'Намет',
+  description: 'Намет "Все для перемоги!"'
+}
 
 export default async function Home() {
   const tokenIsValid = await validateToken()
