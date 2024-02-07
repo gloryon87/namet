7eb63fad8a0582eab04fdcf3028416c5cb66fd8f
Slava Tiutiunnyk <gloryon87@gmail.com>
2024-02-07 17:33:55 +0200

    fixed fethcparams for fetchColors

--- src/app/API/fetchColors.js
+++ src/app/API/fetchColors.js
@@ -1,9 +1,12 @@
 const url = process.env.REACT_APP_SERVER_URL || ''
+import { fetchParamsServer } from '@/app/API/fetchParamsServer'
+
 
 
 async function fetchColors() {
+  const fetchParams = fetchParamsServer()
     try {
-      const response = await fetch(`${url}/api/colors`)
+      const response = await fetch(`${url}/api/colors`, fetchParams)
       const data = await response.json();
       const colorsArray = data.map(color => color.name)
       return colorsArray;
